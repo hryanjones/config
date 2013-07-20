2d60a60a2367973d7bfc7ed89653220a1a1b40c3
H. Ryan Jones <hrjones@amazon.com>
2013-07-19 21:56:46 -0700

    simple install file

--- install.sh
+++ install.sh
@@ -2,7 +2,16 @@
 
 # stolen from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DEST="$HOME"
+BACKUP_DIR="/tmp/backup/`date +%s0`"
 
+mkdir -p $DEST
+mkdir -p $BACKUP_DIR
 cd $DIR
 
-THINGS="`ls `
+THINGS="`ls -A | grep -E "^\." | grep -vE '^\.git(modules)$'`"
+
+for T in $THINGS; do
+  cp -ruv $DEST/$T $BACKUP
+  cp -ruv $T $DEST
+done
