0413e9be4e66505342273c7375c43efb17464216
H. Ryan Jones <hryanjones@ekyuma-kya-okupa.(none)>
2013-07-27 03:24:08 -0700

    New version of install that uses links instead of copying over files.

--- install_config_files.sh
+++ install_config_files.sh
@@ -9,9 +9,8 @@ mkdir -p $DEST
 mkdir -p $BACKUP_DIR
 cd $DIR
 
-FILES=`find . -type f -wholename "./.*" | sed -e 's/^\.\///' | grep -vE "^.git(/|modules|ignore)" | head -3`
-echo "$FILES"
+FILES=`find . -type f -wholename "./.*" | sed -e 's/^\.\///' | grep -vE "^.git(/|modules|ignore)"`
 for FILE in $FILES; do
   mkdir -pv $DEST/`dirname "$FILE"`
-  ln -sv --backup=numbered "$DIR/$FILE" "$DEST/$FILE"
+  ln -sv --backup=numbered -T "$DIR/$FILE" "$DEST/$FILE"
 done
